f87d54884e6d794671014882b4f187c0c074e7c0
flashlab <zhengjuefei25@163.com>
2024-04-08 13:48:02 +0800

    update

--- scripts/filters/post.js
+++ scripts/filters/post.js
@@ -6,7 +6,7 @@ hexo.extend.filter.register('after_post_render', data => {
   const { config } = hexo;
   const theme = hexo.theme.config;
   if (theme.lazyload) {
-    data.content = data.content.replace(/(<img[^>]*) src=/img, '$1 data-src=');
+    data.content = data.content.replace(/(<img[^>]*) src="\/\./img, '$1 data-src="' + theme.vendors.tuchuang);
   }
 
 }, 0);
